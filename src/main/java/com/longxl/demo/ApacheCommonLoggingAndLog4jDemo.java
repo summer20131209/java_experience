5b85bdf9ce4de99d72f93a2914f9da4938b7debc
longxl <longxl2020@163.com>
2022-01-15 20:31:50 +0800

    apache common logging+log4j实现每日生成一个日志文件

--- src/main/java/com/longxl/demo/ApacheCommonLoggingAndLog4jDemo.java
+++ src/main/java/com/longxl/demo/ApacheCommonLoggingAndLog4jDemo.java
@@ -4,18 +4,18 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * 参考：https://blog.csdn.net/geekun/article/details/51398621
+ * Java使用Log日志系统（common-logging和log4j）参考：https://blog.csdn.net/geekun/article/details/51398621
  * log4j.rootLogger配置参考：https://www.cnblogs.com/renjiaqi/p/12192921.html
  */
 public class ApacheCommonLoggingAndLog4jDemo {
     private static Log logger = LogFactory.getLog(ApacheCommonLoggingAndLog4jDemo.class);
 
     public static void main(String[] args) {
-        logger.trace("我是trace信息");
-        logger.debug("我是debug信息");
-        logger.info("我是info信息");
-        logger.warn("我是warn信息");
-        logger.error("我是error信息");
-        logger.fatal("我是fatal信息");
+        logger.trace("我是trace信息,我是trace信息,我是trace信息,我是trace信息,我是trace信息,我是trace信息,,我是trace信息,我是trace信息,我是trace信息,我是t");
+        logger.debug("我是debug信息,我是debug信息,我是debug信息,我是debug信息,我是debug信息,我是debug信息,我是debug信息,我是debug信息,我是debug信");
+        logger.info("我是info信息,我是info信息,我是info信息,我是info信息,我是info信息,我是info信息,我是info信息info信息,我是info信息,我是info信息,我是info信息,我是info信息,我是info");
+        logger.warn("我是warn信息,我是warn信息,我是warn信息,我是warn信息,我是warn信息,我是warn信息,我是warn信息,我是warn信息,我是warn信息");
+        logger.error("我是error信息,我是error信息,我是error信息,我是error信息,我是error信息,我是error信息,我是error信息,我是error信息,我是erro");
+        logger.fatal("我是fatal信息，我是fatal信息，我是fatal信息，我是fatal信息，我是fatal信息，我是fatal信息，我是fatal信息，我是fatal信息，我");
     }
 }
